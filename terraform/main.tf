17b2800422ace01ad2e5b7f4ce8fb4f64c108c47
JulianMKhoo <julian.m.khoo@gmail.com>
2026-02-12 01:49:10 +0700

    init project

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path    = "~/.kube/config"
+    config_context = "minikube"
+  }
+}
+
+resource "kubernetes_namespace_v1" "driveway" {
+  metadata {
+    name = "driveway"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  set = {
+    name  = "server.service.type"
+    value = "NodePort"
+  }
+}
